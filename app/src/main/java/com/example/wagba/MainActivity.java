aa43d1c97dbcd4107d93eb583e0036722fc0ffe5
MostafaXLotfy <mostafaXlotfy@gmail.com>
2022-11-28 11:14:35 +0200

    added layout for register page

--- app/src/main/java/com/example/wagba/MainActivity.java
+++ app/src/main/java/com/example/wagba/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myIntent = new Intent(getApplicationContext(), LoginActivity.class);
+        myIntent = new Intent(getApplicationContext(), RegisterActivity.class);
         startActivity(myIntent);
     }
 }
